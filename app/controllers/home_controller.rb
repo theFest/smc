989ac8fbb49c2eed2acebb3078d8d1a84950b373
Fest White <stjepanb83@gmail.com>
2021-05-22 23:32:19 +0200

    Initialmvc

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,20 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def watchlist
+  end
+
+  def top10
+  end
+
+  def mostcolleted
+  end
+
+  def personalwishlist
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  get 'welcome/index' ## our default rouce for now.
+  get 'home/index'
+  get 'home/about'
+  get 'home/watchlist'
+  get 'home/top10'
+  get 'home/mostcollected'
+  get 'home/personalwishlist'
+  #get 'welcome/index' ## our default rouce for now.
+  #resources :articles
   root 'welcome#index' ## reason, so we can have this route in root path.
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
